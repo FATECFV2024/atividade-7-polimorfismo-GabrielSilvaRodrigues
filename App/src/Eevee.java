9d5b19a94d9a0e731d83a6d97afc522e2316a9bc
Gabriel da Silva <gabriel.rodrigues106@fatec.sp.gov.br>
2024-12-03 08:38:01 -0300

    Add files via upload

---
+++ App/src/Eevee.java
@@ -0,0 +1,17 @@
+public class Eevee{
+    String tipo;
+    Double peso;
+    int altura;
+    int hp;
+    String subtitulo;
+    // mesmo que hit point (ou pontos de saúde)
+    public String ataque(){
+        return "Ataque rápido";
+    }
+    public String especial(){
+        return "Tri-ataque";
+    }
+    public String defesa(){
+        return "Desvio";
+    }
+}
---
+++ App/src/Jolteon.java
@@ -0,0 +1,14 @@
+public class Jolteon extends Eevee{
+    @Override
+    public String ataque(){
+        return "Trovoada - Thunder Shock";
+    }
+    @Override
+    public String especial(){
+        return "Raio - Thunderbold";
+    }
+    @Override
+    public String defesa(){
+        return "Carga - Wild Charge";
+    }
+}
